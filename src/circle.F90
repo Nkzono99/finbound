f4fa6854d71eaa2cdb37a59467f34828f1a368f4
Nkzono99 <71783375+Nkzono99@users.noreply.github.com>
2022-01-01 02:11:53 +0900

    Add: Add base programs.

---
+++ src/circle.F90
@@ -0,0 +1,128 @@
+module m_circle_boundary
+    use m_boundary_base
+    implicit none
+
+    type, extends(t_Boundary) :: t_CircleXYZ
+        !> Axis (1: x, 2: y, 3: z)
+        integer :: axis
+        !> Original position.
+        double precision :: origin(3)
+        double precision :: radius
+
+    contains
+        procedure check_collision => circleXYZ_check_collision
+        procedure is_overlap => circleXYZ_is_overlap
+    end type
+
+    private
+
+    public t_CircleXYZ
+    public new_CircleXYZ
+    public new_CircleX
+    public new_CircleY
+    public new_CircleZ
+
+contains
+
+    pure function new_CircleXYZ(axis, origin, radius) result(obj)
+        integer, intent(in) :: axis
+        double precision, intent(in) :: origin
+        double precision, intent(in) :: radius
+        type(t_CircleXYZ) :: obj
+
+        obj%axis = axis
+        obj%origin = origin
+        obj%radius = radius
+    end function
+
+    pure function new_CircleX(origin, radius) result(obj)
+        double precision, intent(in) :: origin
+        double precision, intent(in) :: radius
+        type(t_CircleXYZ) :: obj
+
+        obj = new_CircleXYZ(1, origin, radius)
+    end function
+
+    pure function new_CircleY(origin, radius) result(obj)
+        double precision, intent(in) :: origin
+        double precision, intent(in) :: radius
+        type(t_CircleXYZ) :: obj
+
+        obj = new_CircleXYZ(2, origin, radius)
+    end function
+
+    pure function new_CircleZ(origin, radius) result(obj)
+        double precision, intent(in) :: origin
+        double precision, intent(in) :: radius
+        type(t_CircleXYZ) :: obj
+
+        obj = new_CircleXYZ(3, origin, radius)
+    end function
+
+    pure function circleXYZ_check_collision(self, p1, p2) result(record)
+        class(t_circleXYZ), intent(in) :: self
+        double precision, intent(in) :: p1(3)
+        double precision, intent(in) :: p2(3)
+        type(t_CollisionRecord) :: record
+
+        double precision :: d1, d2
+        double precision :: r
+        double precision :: pos_collided(3)
+        double precision :: r1, r2
+        integer :: axis1, axis2
+
+        d1 = p1(self%axis) - self%origin(self%axis)
+        d2 = p2(self%axis) - self%origin(self%axis)
+        if (d1*d2 >= 0) then
+            record%is_collided = .false.
+            return
+        end if
+
+        r = abs(d1)/(abs(d1) + abs(d2))
+        pos_collided = (p2 - p1)*r + p1
+
+        axis1 = mod(self%axis + 1, 3) + 1
+        axis2 = mod(self%axis + 2, 3) + 1
+
+        r1 = pos_collided(axis1) - self%origin(axis1)
+        r2 = pos_collided(axis2) - self%origin(axis2)
+        if (r1*r1 + r2*r2 > self%radius*self%radius) then
+            record%is_collided = .false.
+            return
+        end if
+
+        record%is_collided = .true.
+        record%t = r
+        record%position = pos_collided
+    end function
+
+    pure function circleXYZ_is_overlap(self, sdoms) result(is_overlap)
+        class(t_circleXYZ), intent(in) :: self
+        double precision, intent(in) :: sdoms(2, 3)
+        logical :: is_overlap
+
+        integer :: axis0, axis1, axis2
+
+        axis0 = self%axis
+        axis1 = mod(axis0 + 1, 3) + 1
+        axis2 = mod(axis0 + 2, 3) + 1
+
+        if (self%origin(axis0) < sdoms(1, axis0) - 1 &
+            .or. sdoms(2, axis0) + 1 < self%origin(axis0)) then
+            is_overlap = .false.
+        end if
+
+        if (self%origin(axis1) + self%radius < sdoms(1, axis1) - 1 &
+            .or. sdoms(2, axis1) + 1 < self%origin(axis1)) then
+            is_overlap = .false.
+        end if
+
+        if (self%origin(axis2) + self%radius < sdoms(1, axis2) - 1 &
+            .or. sdoms(2, axis2) + 1 < self%origin(axis2)) then
+            is_overlap = .false.
+        end if
+
+        is_overlap = .true.
+    end function
+
+end module
---
+++ src/cylinder.F90
@@ -0,0 +1,160 @@
+module m_cylinder_boundary
+    use m_vector
+    use m_boundary_base
+    implicit none
+
+    !> The cylinder boundary.
+    !
+    !  ^ axis
+    !  |  ______
+    !　 ｨ       ｀ヽ
+    !  |'.______=.'|
+    !  |           |
+    !  |           |
+    !  |           |  height
+    !  |   r       |
+    !  |-----.origin
+    !  '.＿＿＿＿_.'  -> axis+1 or axis+2
+    type, extends(t_Boundary) :: t_CylinderXYZ
+        integer :: axis
+        double precision :: origin(3)
+        double precision :: radius
+        double precision :: height
+    contains
+        procedure check_collision => cylinderXYZ_check_collision
+        procedure is_overlap => cylinderXYZ_is_overlap
+    end type
+
+    private
+    public t_CylinderXYZ
+    public new_cylinderXYZ
+
+contains
+
+    pure function new_cylinderXYZ(axis, origin, radius, height) result(obj)
+        integer, intent(in) :: axis
+        double precision, intent(in) :: origin(3)
+        double precision, intent(in) :: radius
+        double precision, intent(in) :: height
+        type(t_CylinderXYZ) :: obj
+
+        obj%axis = axis
+        obj%origin = origin
+        obj%radius = radius
+        obj%height = height
+    end function
+
+    pure function new_cylinderX(origin, radius, height) result(obj)
+        double precision, intent(in) :: origin(3)
+        double precision, intent(in) :: radius
+        double precision, intent(in) :: height
+        type(t_CylinderXYZ) :: obj
+
+        obj = new_cylinderXYZ(1, origin, radius, height)
+    end function
+
+    pure function new_cylinderY(origin, radius, height) result(obj)
+        double precision, intent(in) :: origin(3)
+        double precision, intent(in) :: radius
+        double precision, intent(in) :: height
+        type(t_CylinderXYZ) :: obj
+
+        obj = new_cylinderXYZ(2, origin, radius, height)
+    end function
+
+    pure function new_cylinderZ(origin, radius, height) result(obj)
+        double precision, intent(in) :: origin(3)
+        double precision, intent(in) :: radius
+        double precision, intent(in) :: height
+        type(t_CylinderXYZ) :: obj
+
+        obj = new_cylinderXYZ(3, origin, radius, height)
+    end function
+
+    pure function cylinderXYZ_check_collision(self, p1, p2) result(record)
+        class(t_CylinderXYZ), intent(in) :: self
+        double precision, intent(in) :: p1(3)
+        double precision, intent(in) :: p2(3)
+        type(t_CollisionRecord) :: record
+
+        integer :: axis0, axis1, axis2
+        double precision :: x1, y1, x2, y2
+        double precision :: a, b, c
+        double precision :: d, d2
+        double precision :: r
+        double precision :: pos_collided(3)
+
+        axis0 = self%axis
+        axis1 = mod(self%axis + 1, 3) + 1
+        axis2 = mod(self%axis + 2, 3) + 1
+
+        x1 = p1(axis1) - self%origin(axis1)
+        y1 = p1(axis2) - self%origin(axis2)
+        x2 = p2(axis1) - self%origin(axis1)
+        y2 = p2(axis2) - self%origin(axis2)
+
+        a = x1*x1 + y1*y1 + x2*x2 + y2*y2 - 2*x1*x2 - 2*y1*y2
+        b = x1*x2 + y1*y2 - x1*x1 - y1*y1
+        c = x1*x1 + y1*y1 - self%radius*self%radius
+
+        d2 = b*b - a*c
+        if (d2 < 0.0d0) then
+            record%is_collided = .false.
+            return
+        end if
+
+        d = sqrt(d2)
+        r = (-b - d)/a
+        if (r < 0.0d0 .or. 1.0d0 < r) then
+            r = (-b + d)/a
+        end if
+        if (r < 0.0d0 .or. 1.0d0 < r) then
+            record%is_collided = .false.
+            return
+        end if
+
+        pos_collided = (p2 - p1)*r + p1
+
+        if (pos_collided(axis0) < self%origin(axis0) &
+            .or. self%origin(axis0) + self%height < pos_collided(axis0)) then
+            record%is_collided = .false.
+            return
+        end if
+
+        record%is_collided = .true.
+        record%t = r
+        record%position = pos_collided
+    end function
+
+    pure function cylinderXYZ_is_overlap(self, sdoms) result(is_overlap)
+        class(t_CylinderXYZ), intent(in) :: self
+        double precision, intent(in) :: sdoms(2, 3)
+        logical :: is_overlap
+
+        integer :: axis0, axis1, axis2
+
+        axis0 = self%axis
+        axis1 = mod(self%axis + 1, 3) + 1
+        axis2 = mod(self%axis + 2, 3) + 1
+
+        if (self%origin(axis0) + self%height < sdoms(1, axis0) - 1 &
+            .or. sdoms(2, axis0) + 1 < self%origin(axis0)) then
+            is_overlap = .false.
+            return
+        end if
+
+        if (self%origin(axis1) + self%radius < sdoms(1, axis1) - 1 &
+            .or. sdoms(2, axis1) < self%origin(axis1) - self%radius) then
+            is_overlap = .false.
+            return
+        end if
+
+        if (self%origin(axis2) + self%radius < sdoms(1, axis2) - 1 &
+            .or. sdoms(2, axis2) < self%origin(axis2) - self%radius) then
+            is_overlap = .false.
+            return
+        end if
+
+        is_overlap = .true.
+    end function
+end module

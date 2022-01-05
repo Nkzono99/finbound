5fa87839908e97d4adacd9585cdf637326d0b5ec
Nkzono99 <71783375+Nkzono99@users.noreply.github.com>
2022-01-05 16:27:29 +0900

    Update: Repository is managed by fpm.

---
+++ src/finbound.f90
@@ -0,0 +1,9 @@
+module finbound
+  use m_boundary_base
+  use m_plane_boundary
+  use m_plane_with_hole_boundary
+  use m_rectangle_boundary
+  use m_circle_boundary
+  use m_cylinder_boundary
+  implicit none
+end module finbound
